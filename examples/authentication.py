da542ebfc1fdc42b641ceaf74ef662a178a35440
Clemente Borges <clemente.borges@desy.de>
2022-03-15 14:28:00 +0100

    Added authentication example

---
+++ examples/authentication.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, '..')
+from desydoorauth import DesyDoorAuth
+
+client = DesyDoorAuth()
+
+auth = client.login("username", "password")
+# If user is authenticated
+print(auth)
+if auth:
+    user_id = auth[1]
+    # Request the user roles
+    roles = client.get_user_roles(user_id)
+    print(roles)
