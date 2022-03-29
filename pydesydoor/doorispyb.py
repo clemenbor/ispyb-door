a84c3409b130bce65c8306d81259a2b70a142e2d
Clemente Borges <clemente.borges@desy.de>
2022-03-29 16:10:19 +0200

    Added get_sessions_to_ispyb method

--- pydesydoor/doorispyb.py
+++ pydesydoor/doorispyb.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from pydesydoor.desydoorapi import DesyDoorAPI
 
 
@@ -7,9 +8,24 @@ class DoorISPyB(DesyDoorAPI):
     RESTful Web-service API client to generate the data format required to import
     data into ISPyB.
     """
+    def get_full_proposal_to_ispyb(self, door_proposal_id, with_sessions=True):
+        ispyb_proposal = {}
+        # Getting the proposal data without leader and cowriters
+        proposal_data = self.get_proposal_to_ispyb(door_proposal_id, False, False)
+        ispyb_proposal["proposal"] = proposal_data
+        if with_sessions:
+            sessions_data = self.get_sessions_to_ispyb(door_proposal_id)
+            ispyb_proposal["sessions"] = sessions_data
+        return json.dumps(ispyb_proposal, indent=4, sort_keys=True, default=str)
 
     def get_proposal_to_ispyb(self, door_proposal_id, with_leader=True, with_cowriters=True):
-        ispyb_proposal = {}
+        """
+           Send a message to a recipient
+
+           :param str door_proposal_id: The DOOR proposal id
+           :param boolean with_leader: True/False depending if the leader data is needed
+           :param boolean with_cowriters: True/False depending if the cowriters data is needed
+        """
         data = {}
         door_proposal = self.get_proposal(door_proposal_id)
         data["title"] = door_proposal["title"]
@@ -24,9 +40,6 @@ class DoorISPyB(DesyDoorAPI):
             pi = self.get_user_to_ispyb(door_proposal["proposalPI"])
             pi["type"] = "pi"
             participants.append(pi)
-        else:
-            # If proposal has no pi return None (pi is needed)
-            return None
         if with_leader:
             # Set the Leader
             if door_proposal["proposalLeader"]:
@@ -36,7 +49,7 @@ class DoorISPyB(DesyDoorAPI):
         if with_cowriters:
             # Set the co-writers
             if door_proposal["proposalCowriters"]:
-                cowriter_ids = [x.strip() for x in door_proposal["proposalCowriters"].split(',')]
+                cowriter_ids = self.split_multiple_by_comma(door_proposal["proposalCowriters"])
                 for cowriter_id in cowriter_ids:
                     cowriter = self.get_user_to_ispyb(cowriter_id)
                     cowriter["type"] = "cowriter"
@@ -44,9 +57,7 @@ class DoorISPyB(DesyDoorAPI):
         # Add participants
         data["participants"] = participants
         # Add proposal data
-        ispyb_proposal["proposal"] = data
-        print(json.dumps(ispyb_proposal))
-        return json.dumps(ispyb_proposal)
+        return data
 
     def get_user_to_ispyb(self, door_user_id, with_laboratory=True):
         user = {}
@@ -66,3 +77,48 @@ class DoorISPyB(DesyDoorAPI):
     def get_laboratory_to_ispyb(self, laboratory_id):
         door_laboratory = self.get_institute(laboratory_id)
         return door_laboratory
+
+    def get_sessions_to_ispyb(self, door_proposal_id, with_participants=True):
+        sessions = self.get_proposal_sessions(door_proposal_id)
+        if sessions:
+            for session in sessions:
+                datetime_start = datetime.strptime(sessions[session]["startDate"], '%Y-%m-%d %H:%M:%S')
+                sessions[session]["startDate"] = datetime_start.isoformat()
+                datetime_end = datetime.strptime(sessions[session]["endDate"], '%Y-%m-%d %H:%M:%S')
+                sessions[session]["endDate"] = datetime_end.isoformat()
+                if sessions[session]["beamlineOperator"]:
+                    operator = self.get_user_to_ispyb(sessions[session]["beamlineOperator"], False)
+                    sessions[session]["beamlineOperator"] = operator
+                if with_participants:
+                    participants = []
+                    remotes = self.get_participants(sessions[session]["participants"], "remote")
+                    if remotes:
+                        participants += remotes
+                    on_sites = self.get_participants(sessions[session]["participants"], "on-site")
+                    if on_sites:
+                        participants += on_sites
+                    data_onlys = self.get_participants(sessions[session]["participants"], "data-only")
+                    if data_onlys:
+                        participants += data_onlys
+                    # Add session participants
+                    sessions[session]["participants"] = participants
+            return sessions
+        return None
+
+    def get_participants(self, participants, participant_type):
+        users = []
+        array_participants = None
+        if participants[participant_type]:
+            array_participants = self.split_multiple_by_comma(str(participants[participant_type]))
+        if array_participants:
+            for participant in array_participants:
+                if participant:
+                    participant = self.get_user_to_ispyb(participant, False)
+                    participant["type"] = participant_type
+                    users.append(participant)
+            return users
+
+    @staticmethod
+    def split_multiple_by_comma(multiple_values):
+        data = [x.strip() for x in multiple_values.split(',')]
+        return data
