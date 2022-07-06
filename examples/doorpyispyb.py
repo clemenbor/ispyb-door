8d2ea2c765ec3235ecb29dddf41cd29b710ece25
Clemente Borges <clemente.borges@desy.de>
2022-07-06 14:57:31 +0200

    Updated some examples

--- examples/doorpyispyb.py
+++ examples/doorpyispyb.py
@@ -2,5 +2,5 @@ from pydesydoor.doorpyispyb import DoorPyISPyB
 
 client = DoorPyISPyB()
 
-proposal = client.get_full_proposal_to_pyispyb("20210004", True, True, True, True)
+proposal = client.get_full_proposal_to_pyispyb("20010001", True, True, True, True, "2022-06-01", "2022-07-01")
 print(proposal)
--- examples/sessions.py
+++ examples/sessions.py
@@ -2,9 +2,9 @@ from pydesydoor.desydoorapi import DesyDoorAPI
 
 client = DesyDoorAPI()
 
-# Get the sessions for a given proposal ID
-proposal_sessions = client.get_proposal_sessions("20210046")
+# Get the sessions for a given proposal ID and a date range
+proposal_sessions = client.get_proposal_sessions("20010001", "P11", "2022-01-01", "2022-12-31")
 # Get all the sessions for a beamline
 beamline_sessions = client.get_beamline_sessions("p11")
 # Get the session given a session id
-session = client.get_session("11000694")
+session = client.get_session("11015610")
